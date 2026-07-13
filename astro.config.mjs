27326e8b2be396260f08e0da91cdf94e0c627b64
aimanrahimi45 <aimanrahimi45@gmail.com>
2026-07-13 18:54:57 +0800

    Configure Astro server mode with Vercel serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://amerispro.com',
   base: '/',
   outDir: './dist',
+  output: 'server',
+  adapter: vercel(),
   build: {
-    format: 'file'
+    format: 'directory'
   }
 });
